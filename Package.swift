b35a0d7c2f0b6816d69206461c9329786193f85e
yury-camacho <yury@gatekeeperx.com>
2026-03-04 09:27:24 -0400

    update ref data

--- Package.swift
+++ Package.swift
@@ -8,13 +8,13 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "Devicex",
-            targets: ["DevicexSDK"]
+            name: "DevicexSDK",
+            targets: ["Devicex"]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "DevicexSDK",
+            name: "Devicex",
             url: "https://github.com/gatekeeperx/devicex-ios-distribution/releases/download/1.0.0/Devicex.xcframework.zip",
             checksum: "4778ee8afc873e1e1f7689eade5e73311c86bc92a2c5e5d82e228b04d13032a9"
         )
