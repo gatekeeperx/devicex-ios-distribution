e0bbb346c78ba002978c8be29c60ec9a168e1662
yury-camacho <yury@gatekeeperx.com>
2026-03-04 10:09:50 -0400

    update version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Devicex",
-            url: "https://github.com/gatekeeperx/devicex-ios-distribution/releases/download/1.0.1/Devicex.xcframework.zip",
+            url: "https://github.com/gatekeeperx/devicex-ios-distribution/releases/download/1.0.2/Devicex.xcframework.zip",
             checksum: "b79c1de82345d7050751fe7db99ae5701a442a8b0314cd71d6814ab9ec853d6e"
         )
     ]
