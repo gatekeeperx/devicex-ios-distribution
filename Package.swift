37ebfa09aed0dffb0b5967d795b4d18177faa822
yury-camacho <yury@gatekeeperx.com>
2026-03-09 16:53:47 -0400

    release 1.0.5
    donwgrade deplaoyment target 17.0
    compile version xcode 16.2

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Devicex",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v17)
     ],
     products: [
         .library(
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Devicex",
-            url: "https://github.com/gatekeeperx/devicex-ios-distribution/releases/download/1.0.4/Devicex.xcframework.zip",
-            checksum: "d6727188197efebc10bb967d2cf9d94bafc60705944cebf3f911e891981412d4"
+            url: "https://github.com/gatekeeperx/devicex-ios-distribution/releases/download/1.0.5/Devicex.xcframework.zip",
+            checksum: "804227c8373220e0a33e46437d0de26be13759703072e7f3a5ee94b035b69f8e"
         )
     ]
 )
