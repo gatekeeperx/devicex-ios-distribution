ee1f56bb8be2c5008899394c21fe5e4787c7b61a
yury-camacho <yury@gatekeeperx.com>
2026-04-18 17:15:31 -0400

    chore: release version 1.0.11

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "DeviceX",
             url: "https://github.com/gatekeeperx/devicex-ios-distribution/releases/download/1.0.11/Devicex.xcframework.zip",
-            checksum: "6248de171aa31f52b90a4b26f80746489d8ff21624c8615d27d35c235c26f583"
+            checksum: "66a1084c680615349eead572982be8cba722c5e8d4bf8a07538b3631aff02e0a"
         )
     ]
 )
